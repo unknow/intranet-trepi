975a5bd0f12d4a90f52ce520a82ae7ed0b6d2a22
Stanley Andrade <stanley.andrade@etipi.pi.gov.br>
2025-12-01 09:57:34 -0300

    Exercício 21: Passo de atualização para o tipo Área

--- backend/src/trepi/intranet/setuphandlers/__init__.py
+++ backend/src/trepi/intranet/setuphandlers/__init__.py
@@ -1,4 +1,8 @@
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFPlone.WorkflowTool import WorkflowTool
+from Products.GenericSetup.tool import SetupTool
+from trepi.intranet import logger
 from zope.interface import implementer
 
 
@@ -15,3 +19,11 @@ class HiddenProfiles:
         return [
             "trepi.intranet.upgrades",
         ]
+
+
+def fecha_intranet(portal_setup: SetupTool):
+    """Aplica novo workflow para a intranet."""
+    wf_tool: WorkflowTool = api.portal.get_tool("portal_workflow")
+    wf_tool.updateRoleMappings()
+    # Loga que modificação foi realizada
+    logger.info("Permissões de workflow atualizadas")
