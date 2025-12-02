55b5948cd6a69199b82625a2f0a4cbef27d65f13
Stanley Andrade <stanley.andrade@etipi.pi.gov.br>
2025-12-02 12:54:40 -0300

    Exercício 34: Criar scripts para interagir com a API do Plone

--- frontend/packages/volto-trepi-intranet/src/components/Endereco/Endereco.tsx
+++ frontend/packages/volto-trepi-intranet/src/components/Endereco/Endereco.tsx
@@ -11,25 +11,27 @@ const Endereco: React.FC<EnderecoProps> = ({ content }) => {
 
   return (
     <Container narrow className="endereco">
-      <Container className="logradouro">
-        <span className="label">Endereco</span>:{' '}
-        <span className="value">{endereco}</span>
-      </Container>
-      <Container className="complemento">
-        <span className="label">Complemento</span>:{' '}
-        <span className="value">{complemento}</span>
-      </Container>
-      <Container className="estado">
-        <span className="label">Estado</span>:{' '}
-        <span className="value">{estado.token}</span>
-      </Container>
-      <Container className="cidade">
-        <span className="label">Cidade</span>:{' '}
-        <span className="value">{cidade}</span>
-      </Container>
-      <Container className="cep">
-        <span className="label">CEP</span>: <span className="value">{cep}</span>
-      </Container>
+      {endereco && (
+        <Container>
+          <span className="endereco">{endereco}</span>
+        </Container>
+      )}
+      {complemento && (
+        <Container>
+          <span className="complemento">{complemento}</span>
+        </Container>
+      )}
+      {cidade && estado && (
+        <Container>
+          <span className="cidade">{cidade}</span> {' - '}
+          <span className="estado">{estado.token}</span>
+        </Container>
+      )}
+      {cep && (
+        <Container>
+          <span className="cep">{cep}</span>
+        </Container>
+      )}
     </Container>
   );
 };
