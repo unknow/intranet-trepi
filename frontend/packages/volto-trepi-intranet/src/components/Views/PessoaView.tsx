98a166848290775a9271a79484fd402a89376366
Stanley Andrade <stanley.andrade@etipi.pi.gov.br>
2025-12-02 11:53:48 -0300

    Exercício 31: Criação do tipo Pessoa

---
+++ frontend/packages/volto-trepi-intranet/src/components/Views/PessoaView.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from '@plone/volto/components/theme/Image/Image';
+import { Container } from '@plone/components';
+import ContactInfo from 'volto-trepi-intranet/components/ContactInfo/ContactInfo';
+import EnderecoInfo from 'volto-trepi-intranet/components/Endereco/Endereco';
+import type { Pessoa } from 'volto-trepi-intranet/types/content';
+
+interface PessoaViewProps {
+  content: Pessoa;
+  [key: string]: any;
+}
+
+const PessoaView: React.FC<PessoaViewProps> = (props) => {
+  const { content } = props;
+
+  return (
+    <Container id="page-document" narrow className="view-wrapper area-view">
+      {content.image && (
+        <Container className={'image'}>
+          <Image
+            className="documentImage ui right floated image"
+            alt={content.title}
+            title={content.title}
+            item={content}
+            imageField="image"
+            responsive={true}
+          />
+        </Container>
+      )}
+      <h1 className="documentFirstHeading">{content.title}</h1>
+      {content.description && (
+        <p className="documentDescription">{content.description}</p>
+      )}
+      <ContactInfo content={content} />
+      <EnderecoInfo content={content} />
+    </Container>
+  );
+};
+
+export default PessoaView;
--- frontend/packages/volto-trepi-intranet/src/types/content.ts
+++ frontend/packages/volto-trepi-intranet/src/types/content.ts
@@ -14,3 +14,18 @@ export interface Area extends Content {
   };
   cep?: string;
 }
+
+export interface Pessoa extends Content {
+  title: string;
+  description: string;
+  telefone?: string;
+  email?: string;
+  endereco?: string;
+  complemento?: string;
+  cidade?: string;
+  estado?: {
+    token: string;
+    title: string;
+  };
+  cep?: string;
+}
